94a1d8014f0233e4f4c852ec49786be425af3054
LLYEmily <98926514+LLYEmily@users.noreply.github.com>
2023-01-01 20:43:11 -0800

    Add files via upload

---
+++ FriendBook/src/main/java/friendbook/friendbook/Controller.java
@@ -0,0 +1,60 @@
+package friendbook.friendbook;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class Controller {
+    public TextField textGetName;
+    public TextField textGetAge;
+    public Label lblName;
+    public Label lblAge;
+    public Label lblBirthday;
+    public Label lblFC;
+    public TextField textGetBirthday;
+    public TextField textGetFC;
+    public ListView<Friend> FriendListD = new ListView<>();
+    public ListView<Friend> FriendListE = new ListView<>();
+
+    //create method to add friend
+    public void addFriend(ActionEvent actionEvent) {
+        String name = textGetName.getText();
+        int age = Integer.parseInt(textGetAge.getText());
+        String birthday = textGetBirthday.getText();
+        String FC = textGetFC.getText();
+        Friend temp = new Friend (name, age, birthday, FC);
+        FriendListD.getItems().add(temp);
+        FriendListE.getItems().add(temp);
+        textGetAge.clear();
+        textGetName.clear();
+        textGetBirthday.clear();
+        textGetFC.clear();
+    }
+    //to view the Friends List
+    public void viewList(MouseEvent mouseEvent) {
+        Friend temp;
+        temp = FriendListD.getSelectionModel().getSelectedItem();
+        lblName.setText(temp.getName());
+        lblAge.setText(Integer.toString(temp.getAge()));
+        lblBirthday.setText(temp.getBirthday());
+        lblFC.setText(temp.getFavouriteColor());
+    }
+    //chose the friend from the list
+    public void choseFriend(MouseEvent mouseEvent) {
+        Friend temp;
+        temp = FriendListE.getSelectionModel().getSelectedItem();
+    }
+    //method to remove friends from the list
+    public void removeFriend(ActionEvent actionEvent) {
+        Friend temp;
+        temp = FriendListE.getSelectionModel().getSelectedItem();
+        FriendListD.getItems().remove(temp);
+        FriendListE.getItems().remove(temp);
+        lblName.setText("");
+        lblAge.setText("");
+        lblBirthday.setText("");
+        lblFC.setText("");
+    }
+}
