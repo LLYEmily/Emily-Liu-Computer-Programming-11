0b85d9553cbc8a8bffcbe5e301ad3f2f82d7321b
LLYEmily <98926514+LLYEmily@users.noreply.github.com>
2023-02-11 21:24:24 -0800

    Update Main.java

--- ReadingText/src/Main.java
+++ ReadingText/src/Main.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 public class Main {
 //Method to search the word
+// Requires: string
+// Modifies: search a word in text and display its index position and line position
+// Effects: method to find word
     public static int Search(String finding) throws IOException{
         FileReader fr = new FileReader("in.txt");
         BufferedReader br = new BufferedReader(fr);
@@ -17,14 +20,16 @@ public class Main {
             lineCounter++;
             indexBeginning = line.indexOf(finding);
             while(indexBeginning >= 0){
-                System.out.println("Found word: " + finding + ", at index position: " + indexBeginning + ", on Array: " + lineCounter);
+                System.out.println("Found word: " + finding + ", at index position: " + indexBeginning + ", in line: " + lineCounter);
                 indexBeginning = line.indexOf(finding, indexBeginning + 1);
                 count++;
             }
         }
         return indexBeginning;
     }
-
+// Requires: string
+// Modifies: create in.txt and out.txt, story the information in in.txt, search the word
+// Effects: find the word
     public static void main(String[] args) throws IOException{
         //copies each line into an ArrayList
         ArrayList<String> lines = new ArrayList<>();
@@ -44,9 +49,9 @@ public class Main {
         }
         bw.close();
         //runs the method that finds the text
-        Scanner s = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         System.out.println("Enter the word you want to search");
-        String a = s.nextLine();
-        Search(a);
+        String x = scan.nextLine();
+        Search(x);
     }
 }
